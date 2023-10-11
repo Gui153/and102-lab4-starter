74c1d676ed91a5eafc830e4021a44c9e41fef794
Guilherme Vilatoro Taglianeti <gv8@njit.edu>
2023-10-11 16:11:09 -0400

    Lab 5 completed

--- app/src/main/java/com/codepath/articlesearch/DetailActivity.kt
+++ app/src/main/java/com/codepath/articlesearch/DetailActivity.kt
@@ -1,6 +1,7 @@
 package com.codepath.articlesearch
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -14,22 +15,48 @@ class DetailActivity : AppCompatActivity() {
     private lateinit var bylineTextView: TextView
     private lateinit var abstractTextView: TextView
 
+
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onContextItemSelected(item)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
-        // TODO: Find the views for the screen
+
+        var actionBar = getSupportActionBar()
+
+        // showing the back button in action bar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+        }
+
+
+    // this event will enable the back
+    // function to the button on press
+
+
+    // TODO: Find the views for the screen
         mediaImageView = findViewById(R.id.mediaImage)
         titleTextView = findViewById(R.id.mediaTitle)
         bylineTextView = findViewById(R.id.mediaByline)
         abstractTextView = findViewById(R.id.mediaAbstract)
 
         // TODO: Get the extra from the Intent
-        val article = intent.getSerializableExtra(ARTICLE_EXTRA) as Article
+        val article = intent.getSerializableExtra(ARTICLE_EXTRA) as DisplayArticle
 
         // TODO: Set the title, byline, and abstract information from the article
-        titleTextView.text = article.headline?.main
-        bylineTextView.text = article.byline?.original
+        titleTextView.text = article.headline
+        bylineTextView.text = article.byline
         abstractTextView.text = article.abstract
 
         // TODO: Load the media image
